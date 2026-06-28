53f713cf044e01b1679c37abe004f7b40fcf2ec3
Kyle <kylewang2655@gmail.com>
2026-06-28 19:29:42 +0800

    fix: correct e5f6a1b2c3d4 down_revision to 3c4d5e6f7a8b

--- backend/app/alembic/versions/e5f6a1b2c3d4_add_email_subject_and_body.py
+++ backend/app/alembic/versions/e5f6a1b2c3d4_add_email_subject_and_body.py
@@ -14,7 +14,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = "e5f6a1b2c3d4"
-down_revision: Union[str, None] = "d4e5f6a1b2c3"
+down_revision: Union[str, None] = "3c4d5e6f7a8b"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
